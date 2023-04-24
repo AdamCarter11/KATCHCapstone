14c28a8605b7ea98d709a794dd434281a5bd7b76
Adam <57573881+AdamCarter11@users.noreply.github.com>
2023-04-24 14:39:18 -0700

    jonathan updated

--- exec/image-object-identification/image_object_identification.py
+++ exec/image-object-identification/image_object_identification.py
@@ -1,6 +1,4 @@
 from jina import Executor, requests, Document, DocumentArray
-
-print("test")
 class ImageObjectIdentification(Executor):
     '''
     This is an example Executor. Here you would add all the functionality needed
