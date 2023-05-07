92bde663ee2a6b043d4414deb90eae512f700183
jjsquadd <jpiyer@ucsc.edu>
2023-05-06 23:23:47 -0700

    GPT-2 is working!

--- exec/gpt_executor/gpt2_interactive.py
+++ exec/gpt_executor/gpt2_interactive.py
@@ -1,12 +1,13 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
 def interact_with_gpt2(prompt):
     model_name = "gpt2"  # You can use "gpt2-medium", "gpt2-large", or "gpt2-xl" for larger models
 
     tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     model = GPT2LMHeadModel.from_pretrained(model_name)
+
+    # Set the pad token to the EOS token
+    tokenizer.pad_token = tokenizer.eos_token
 
     inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=150)
     outputs = model.generate(**inputs, max_length=150, num_return_sequences=1)
@@ -19,4 +20,3 @@ if __name__ == "__main__":
         user_input = input("Enter a prompt for GPT-2: ")
         response = interact_with_gpt2(user_input)
         print("GPT-2 Response:", response)
-
